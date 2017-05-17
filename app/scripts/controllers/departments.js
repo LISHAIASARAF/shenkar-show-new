0ad9888119a1dc074f684024556528041afa1c19
lishai <lishai0812@gmail.com>
2017-05-17 10:44:43 +0300

    fix admin api

--- app/scripts/controllers/departments.js
+++ app/scripts/controllers/departments.js
@@ -11,7 +11,7 @@ angular.module('sbAdminApp')
         $scope.departments = [];
         if (!$rootScope.user) {
             $state.go('login');
-        }else {
+        } else {
             $scope.me = $rootScope.user;
             if ($scope.me.role != 'institute manager') {
                 alert('אין לך הרשאה');
@@ -105,4 +105,19 @@ angular.module('sbAdminApp')
             });
         }
 
-    });
+    }).directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                scope.$apply(function () {
+                    scope.fileread = changeEvent.target.files[0];
+                    // or all selected files:
+                    // scope.fileread = changeEvent.target.files;
+                });
+            });
+        }
+    }
+}]);

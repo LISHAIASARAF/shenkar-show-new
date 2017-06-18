77402208db6f24d8fe181b0c3d9dc42b0ea4f635
lishai <lishai0812@gmail.com>
2017-06-18 20:55:15 +0300

    aaaa

--- app/scripts/controllers/locations.js
+++ app/scripts/controllers/locations.js
@@ -4,5 +4,16 @@ angular.module('sbAdminApp')
             $http.get('https://shenkar-show.herokuapp.com/institute/locations').then(function (res) {
                 $scope.locations = res.data;
             });
-        }
+        };
+
+        $scope.setEdit = function (id) {
+            $scope.selected = null;
+
+            $scope.locations.forEach(function (l) {
+                if (l.id == id) {
+                    $scope.selected = angular.copy(l);
+                }
+            });
+
+        };
     });

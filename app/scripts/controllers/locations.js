feb65e3b15f930f8024a6e24e47a82c400b2ad3e
lishai <lishai0812@gmail.com>
2017-06-18 21:14:51 +0300

    aaaa

--- app/scripts/controllers/locations.js
+++ app/scripts/controllers/locations.js
@@ -1,6 +1,7 @@
 angular.module('sbAdminApp')
     .controller('LocationsCtrl', function ($scope, $position, $http, $cookies, $rootScope, $state) {
         $scope.init = function () {
+            $http.defaults.headers.common['X-Access-Token'] = $cookies.shenkarShowUserId;
             $http.get('https://shenkar-show.herokuapp.com/institute/locations').then(function (res) {
                 $scope.locations = res.data;
             });
@@ -14,6 +15,26 @@ angular.module('sbAdminApp')
                     $scope.selected = angular.copy(l);
                 }
             });
-
         };
+
+        $scope.update = function () {
+
+            $scope.selected.institute = $rootScope.user.institute;
+            $http.post('https://shenkar-show.herokuapp.com/institute/updateLocation').then(function () {
+                toastr.info('הנתונים נשמרו בהצלחה');
+                $scope.init();
+                $('.modal').modal('hide');
+            });
+        }
+
+        $scope.create = function () {
+
+            $scope.new.institute = $rootScope.user.institute;
+
+            $http.post('https://shenkar-show.herokuapp.com/institute/createLocation').then(function () {
+                toastr.info('הנתונים נשמרו בהצלחה');
+                $scope.init();
+                $('.modal').modal('hide');
+            });
+        }
     });

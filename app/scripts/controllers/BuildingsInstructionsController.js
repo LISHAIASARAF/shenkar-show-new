d0f58f9bb5ee7bfe342c2d76ec17626ae7711b58
lishai <lishai0812@gmail.com>
2017-09-06 20:42:51 +0300

    dfd

--- app/scripts/controllers/BuildingsInstructionsController.js
+++ app/scripts/controllers/BuildingsInstructionsController.js
@@ -8,5 +8,19 @@
  */
 angular.module('sbAdminApp')
     .controller('BuildingsInstructionCtrl', function ($scope, $rootScope, $state) {
+        $scope.init = function () {
+            if (!$rootScope.user) {
+                $state.go('login');
+            } else {
 
+                $scope.me = $rootScope.user;
+
+                if ($scope.me.role != 'admin') {
+                    alert('אין לך הרשאה');
+                    $state.go('dashboard.home');
+                }
+            }
+
+
+        };
     });
